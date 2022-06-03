fe83d607bd21d1923baea86935cd9500891d16cc
Mash-7 <mashoratile@gmail.com>
2022-06-02 18:40:13 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
-#include<unistd.h>
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{puts("\"Programming is lie building a multilingual puzzle\n"); return (0);}
+{
+	puts("\"Programming is lie building a multilingual puzzle\n"); return (0);
+}
