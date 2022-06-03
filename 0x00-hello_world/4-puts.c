177b8770867a27ae9f643482d63fde6f68574111
Mash-7 <mashoratile@gmail.com>
2022-06-02 18:34:05 -0700

    4-puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
 #include<unistd.h>
 int main(void)
-{
-	puts("\"Programming is lie building a multilingual puzzle\n"); return (0);
-}
+{puts("\"Programming is lie building a multilingual puzzle\n"); return (0);}
