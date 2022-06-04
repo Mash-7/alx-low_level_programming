bcd280fa1856f3a7882168d89f3075319874d109
Mash-7 <mashoratile@gmail.com>
2022-06-03 20:45:08 -0700

    9-print_comb.c commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers 0 to 9 separated by commas and spaces
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
