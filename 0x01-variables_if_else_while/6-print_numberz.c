866ca3753142966437cd91f93704c7f7b32a9415
Mash-7 <mashoratile@gmail.com>
2022-06-03 20:02:27 -0700

    6-print_numberz.c commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers between 0 and 9.
+ *
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 45; i < 56; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
