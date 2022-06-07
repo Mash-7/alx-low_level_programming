0c46974c7d0193cf05e1ca9657d284d3a0c2c081
Mash-7 <mashoratile@gmail.com>
2022-06-06 19:59:40 -0700

    102-print_comb5.c commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Print all combinatons of numbers from 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, k;
+
+	for (i = 0; i <= 98; i++)
+	{
+		for (k = i + 1; k <= 99; k++)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((k / 10) + '0');
+			putchar((k % 10) + '0');
+
+			if (i == 98 && k == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
