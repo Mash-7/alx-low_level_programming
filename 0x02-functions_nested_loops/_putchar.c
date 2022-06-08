dc8d56e4f47081e6b76c2b610b85235bd392c6bb
Mash-7 <mashoratile@gmail.com>
2022-06-07 19:44:37 -0700

    _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,6 +1,13 @@
-#include "main.h"
 #include <unistd.h>
+#include "main.h"
 
+/**
+ * _putchar - writes the character c to standout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * on error, -1 is returned and errno is set appropriately.
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
