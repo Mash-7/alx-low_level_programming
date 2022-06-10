b853c2f6e26d6f70b329a688e1659bf0f63287a8
Mash-7 <mashoratile@gmail.com>
2022-06-09 21:32:56 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
 	for (i = 48; i < 57; i++)
 	{
 		if (i != 50 && i != 52)
-			_putchar((i % 10) + 48);
+			_putchar(i + 48);
 	}
 	_putchar('\n');
 }
