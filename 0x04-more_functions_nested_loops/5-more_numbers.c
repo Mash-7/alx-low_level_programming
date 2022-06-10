09f3dfbd58d0dc67a57e48948d246152e24f034b
Mash-7 <mashoratile@gmail.com>
2022-06-09 22:08:42 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,11 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; i < 10; i++)
+	for (j = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
+			if (j > 9)
 				_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 		}
