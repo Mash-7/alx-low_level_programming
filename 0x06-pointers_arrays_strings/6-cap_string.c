57f028988d451b86a2d7483a3a7823136754ed72
Mash-7 <mashoratile@gmail.com>
2022-06-16 16:47:47 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalzes all words of a string.
+ * @s: The string to be capitalzed.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+
+		if (s[i - 1] == 32 ||
+				s[i - 1] == 33 ||
+		s[i - 1] == 34 ||
+		s[i - 1] == 40 ||
+		s[i - 1] == 41 ||
+		s[i - 1] == 44 ||
+		s[i - 1] == 59 ||
+		s[i - 1] == 63 ||
+		s[i - 1] == 123 ||
+		s[i - 1] == 125 ||
+		s[i - 1] == '\t' ||
+		s[i - 1] == '\n' ||
+		i == 0)
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
