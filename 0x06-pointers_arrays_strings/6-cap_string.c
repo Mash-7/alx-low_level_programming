648e4f2f03983b1b737d6b8e9fd300d6c6fa4c8b
Mash-7 <mashoratile@gmail.com>
2022-06-16 18:55:09 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,33 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalzes all words of a string.
- * @s: The string to be capitalzed.
- *
- * Return: A pointer to the changed string.
+ * cap_string - Capitalizes all words of a string.
+ * @s: input string
+ * Return: The pointer to dest.
  */
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i = 0, j;
 
-	while (s[i])
+	while (s[i] != '\0')
+		i++;
+
+	for (j = 0; j < i; j++)
 	{
-		while (!(s[i] >= 'a' && s[i] <= 'z'))
-			i++;
-
-		if (s[i - 1] == ' ' ||
-				s[i - 1] == '\t' ||
-				s[i - 1] == '\n' ||
-				s[i - 1] == ',' ||
-				s[i - 1] == ';' ||
-				s[i - 1] == '.' ||
-				s[i - 1] == '!' ||
-				s[i - 1] == '?' ||
-				s[i - 1] == '"' ||
-				s[i - 1] == '(' ||
-				s[i - 1] == ')' ||
-				s[i - 1] == '{' ||
-				s[i - 1] == '}' ||
-				i == 0)
-			s[i] -= 32;
-
-	i++;
+		if (j == 0 && s[j] >= 97 && s[j] <= 122)
+			s[e] = s[e] - 32;
+		if (s[j] >= 97 && s[e] <= 122)
+		{
+			if (s[j - 1] == 32 || s[j - 1] == '\t' ||
+					s[j - 1] == '\n' ||
+					s[j - 1] == 44 ||
+					s[j - 1] == 59 || s[j - 1] == 46 ||
+					s[j - 1] == 33 || s[j - 1] == 63 ||
+					s[j - 1] == 34 || s[j - 1] == 40 ||
+					s[j - 1] == 41 || s[j - 1] == 123 ||
+					s[j - 1] == 124)
+				s[j] = s[j] - 32;
+		}
 	}
-	return (s);
+	return (s)
 }
