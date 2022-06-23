f488ca6352aec4d5690554e3d99b6f3fe86c13b5
Mash-7 <mashoratile@gmail.com>
2022-06-22 20:54:26 -0700

    main.h

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Determines whether a string is a palindrome.
+ * @s: String to be checked.
+ *
+ * Return: If s is a palindrome, 1.
+ * If s is not a palindrome, o.
+ */
+int is_palindrome(char *s)
+{
+	if (!(*s))
+	       return (1);
+return (palindrome(s, l, i));
+}
