b956b556fa3ba7b09f1044f200a31b6ef30088cd
Mash-7 <mashoratile@gmail.com>
2022-06-28 20:20:05 -0700

    redo

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Find and print the first 98 Fibonacc numbers,
+ * starting with 1 and 2.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j, k;
+	long int fn1, fn2, fib1, fib2, jk1, jk2;
+
+	fn1 = 1;
+	fn2 = 2;
+	j = k = 1;
+	printf("%ld, %ld", fn1, fn2);
+	for
--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The Length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s)
+	{
+		l++;
+		l += _strlen_recursion(s + 1);
+	}
+	return (l);
+}
+
+/**
+ * palin_check - Determines whether a string is a palindrome.
+ * @s: The string to be checked.
+ * @l: The length of the string.
+ *
+
+/**
  * is_palindrome - Determines whether a string is a palindrome.
  * @s: String to be checked.
  *
