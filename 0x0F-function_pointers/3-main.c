7463e19d64be7e45f7fcd3b355a2bcf5a676bbea
Mash-7 <mashoratile@gmail.com>
2022-07-08 15:46:33 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,11 +18,14 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 
-	if ((*ops == '/' && num2 == 0) ||
-			(*ops == '%' && num2 == 0))
+	num1 = atoi(argv[1]);
+	ops = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (ops == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
 	if (get_op_func(ops) == NULL || ops[1] != '\0')
@@ -30,10 +33,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	ops = argv[2];
 
 	printf("%d\n", get_op_func(ops)(num1, num2));
 	return (0);
