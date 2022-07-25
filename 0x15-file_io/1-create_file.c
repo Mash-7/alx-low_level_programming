74cd733ccc57fbb02008b7d5689c7c44154976f0
Mash-7 <mashoratile@gmail.com>
2022-07-25 16:24:15 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_file - Creates a new file.
+ * @filename: Name of the file to create.
+ * @text_content: A NULL terminated string to write to the file.
+ *
+ * Return: On success, 1.
+ * On failure, -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0, file, wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		for (i = 0; text_content[i];)
+			i++;
+	}
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	wr = write(file, text_content, i);
+
+	if (file == -1)
+		return (-1);
+
+	close(file);
+	return (1);
+}
