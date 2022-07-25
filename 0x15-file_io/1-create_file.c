dc519c07f4b17bcf07ab14f59c47c7f016c0efd4
Mash-7 <mashoratile@gmail.com>
2022-07-25 16:27:49 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,10 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i];)
 			i++;
 	}
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(file, text_content, i);
 
-	if (file == -1)
+	if (file == -1 || wr == -1)
 		return (-1);
 
 	close(file);
