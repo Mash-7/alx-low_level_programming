18fb0215672300c95f4c28f5bd538dc95f89b53f
Mash-7 <mashoratile@gmail.com>
2022-07-25 17:05:26 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,12 +17,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-	while (text_content[i] == '\0')
+	if (text_content)
 	{
-		i++;
+		for (i = 0; text_content[i];)
+			i++;
 	}
 	file = open(filename, O_WRONLY | O_APPEND, 0600);
 	wr = write(file, text_content, wr);
