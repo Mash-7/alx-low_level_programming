e0f8965879f4bb574ad4c585c2b5264ecfcf836a
Mash-7 <mashoratile@gmail.com>
2022-09-01 19:20:13 -0700

    return not returns

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,8 +5,8 @@
  * of a dlistint_t linked list.
  * @head: Pointer to the head of the dlistint_t list.
  *
- * Returns: the sum of the data.
- * If the list i empty, 0.
+ * Return: the sum of the data.
+ * If the list is empty, 0.
  */
 int sum_dlistint(dlistint_t *head)
 {
